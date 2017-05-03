2fe19e7bceb10e66fe72f40ea9dec0e67d74d33d
Bill Kalaitzis <bill.kalaitzis@gmail.com>
2017-05-03 22:22:40 +0300

    show_failed_ssh_logins.sh: Add comments to explain functionality

--- show_failed_ssh_logins.sh
+++ show_failed_ssh_logins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# A script that scans /var/log/auth.log* and lists the top 20 IPs that failed to login via SSH
 
+# Check for root priviledges
 if [ "$(id -u)" -ne "0" ]; then
 	echo "You must run the command with root priviledges."
 	exit 1
